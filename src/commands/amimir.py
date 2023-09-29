fc8c059ee5dddc82f6b30efb709035a79aed8aa8
Lucas Ransan <lucas@ransan.fr>
2023-09-29 13:32:48 +0200

    add amimir command

---
+++ src/commands/amimir.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+from telegram import Update
+from telegram.ext import Application, CommandHandler, CallbackContext
+
+app: Application
+handler: CommandHandler
+
+def init(_app: Application) -> None:
+    global app, handler, db
+    app = _app
+    handler = CommandHandler("amimir", command)
+    app.add_handler(handler)
+
+def deinit() -> None:
+    app.remove_handler(handler)
+
+async def command(update: Update, context: CallbackContext) -> None:
+    with os.scandir("resources/amimir") as it:
+        entry = random.choice(it)
+        await update.effective_message.reply_photo(entry.path)
