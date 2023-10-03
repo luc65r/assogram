d5742c23d25e44cae405969cc93e9cedc141837d
Lucas Ransan <lucas@ransan.fr>
2023-10-03 17:33:09 +0200

    add remaining time and fix deadline

--- src/commands/bts.py
+++ src/commands/bts.py
@@ -1,8 +1,10 @@
 import os
+from datetime import datetime
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+import humanize
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 
@@ -24,6 +26,8 @@ def init(_app: Application) -> None:
     service = build("sheets", "v4", credentials=creds)
     sheet = service.spreadsheets()
 
+    humanize.i18n.activate("fr_FR")
+
     handler = CommandHandler("bts", command)
     app.add_handler(handler)
 
@@ -33,8 +37,10 @@ def deinit() -> None:
 async def command(update: Update, context: CallbackContext) -> None:
     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE).execute()
     [taken, remaining_bus, remaining_party] = result["values"][0]
+    dl = datetime(2023, 10, 5, 21, 0)
+    remaining_time = humanize.precisedelta(dl - datetime.now(), minimum_unit="seconds", format="%0.0f")
     await update.effective_message.reply_html(f"""<b>Soirée Back To School</b>
 Lien d'inscription : {LINK}
 Il reste {remaining_party} places, dont {remaining_bus} pour le bus de 23h.
-Vous avez jusqu'au vendredi 6 octobre à 21h pour vous inscrire.
+Vous avez {remaining_time} (jusqu'au jeudi 5 octobre à 21h) pour vous inscrire.
     """)
