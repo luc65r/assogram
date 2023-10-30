b89d7ef58e7d45bfc17fe27b0fcdd9a7f24dee78
Lucas Ransan <lucas@ransan.fr>
2023-10-30 15:19:07 +0100

    display halloween poster

--- src/commands/halloween.py
+++ src/commands/halloween.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import humanize
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.constants import ParseMode
 
 LINK = os.getenv("HALLOWEEN_LINK")
 
@@ -25,7 +26,10 @@ def deinit() -> None:
 async def command(update: Update, context: CallbackContext) -> None:
     dl = datetime(2023, 11, 10, 21, 0)
     remaining_time = humanize.precisedelta(dl - datetime.now(), minimum_unit="seconds", format="%0.0f")
-    await update.effective_message.reply_html(f"""<b>Soirée Halloween</b>
+    await update.effective_message.reply_photo(
+        "resources/affiche_halloween.jpg",
+        caption=f"""<b>Soirée Halloween</b>
 Lien d'inscription : {LINK}
-Vous avez {remaining_time} (jusqu'au vendredi 10 novembre à 21h) pour vous inscrire.
-    """)
+Vous avez {remaining_time} (jusqu'au vendredi 10 novembre à 21h) pour vous inscrire.""",
+        parse_mode=ParseMode.HTML,
+    )
